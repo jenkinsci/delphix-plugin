c2f976debf11489d8bdf3040478edcd1fe67d3f1
ankit.patil <ankit.patil@delphix.com>
2023-10-16 17:54:59 +0530

    updated version number

--- src/main/java/io/jenkins/plugins/constant/Constant.java
+++ src/main/java/io/jenkins/plugins/constant/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     public static final String UNIQUE_FILE_NAME = "delphix-VDB-";
     public static final String PROPERTIES = ".properties";
     public static final String FILE_NAME = "delphix-VDB";
-    public static final String USER_AGENT = "Jenkins-3.0.0";
+    public static final String USER_AGENT = "Jenkins-3.1.0";
     public static final String CLIENT_NAME = "Jenkins";
     public static final String CLIENT_NAME_HEADER = "x-dct-client-name";
     public static final long WAIT_TIME = 20000;
