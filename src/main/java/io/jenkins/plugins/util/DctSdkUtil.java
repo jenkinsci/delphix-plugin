1297750c43bacdc8c0fa942c6ff6e650897358d0
ankit.patil <ankit.patil@delphix.com>
2024-02-20 15:35:01 +0530

    fix for security vulnerability and enabled testcases

--- src/main/java/io/jenkins/plugins/util/DctSdkUtil.java
+++ src/main/java/io/jenkins/plugins/util/DctSdkUtil.java
@@ -19,108 +19,104 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import io.jenkins.plugins.constant.Constant;
 import io.jenkins.plugins.delphix.DelphixGlobalConfiguration;
+
 import static io.jenkins.plugins.util.CredentialUtil.getApiKey;
+
 import io.jenkins.plugins.delphix.Messages;
 
 public class DctSdkUtil {
 
-    private ApiClient defaultClient;
+  private ApiClient defaultClient;
 
-    public DctSdkUtil(Run<?, ?> run, TaskListener listener, String credId) {
-        String url = DelphixGlobalConfiguration.get().getDctUrl();
-        if (url == null) {
-            listener.getLogger().println(Messages.DctSDkUtil_Error1());
-            return;
-        }
-        String apiKey = getApiKey(credId, run);
-        if (apiKey == null) {
-            listener.getLogger().println(Messages.DctSDkUtil_Error2(credId));
-            return;
-        }
-        this.defaultClient = Configuration.getDefaultApiClient();
-        if (DelphixGlobalConfiguration.get().getSslCheck()) {
-            this.defaultClient.setVerifyingSsl(false);
-        }
-        this.defaultClient.setConnectTimeout(Constant.TIMEOUT);
-        this.defaultClient.setReadTimeout(Constant.TIMEOUT);
-        this.defaultClient.setWriteTimeout(Constant.TIMEOUT);
-        this.defaultClient.setUserAgent(Constant.USER_AGENT);
-        this.defaultClient.addDefaultHeader(Constant.CLIENT_NAME_HEADER, Constant.CLIENT_NAME);
-        this.defaultClient.setApiKey(apiKey);
-        this.defaultClient.setBasePath(url + Constant.API_VERSION);
+  public DctSdkUtil(Run<?, ?> run, TaskListener listener, String credId) {
+    String url = DelphixGlobalConfiguration.get().getDctUrl();
+    if (url == null) {
+      listener.getLogger().println(Messages.DctSDkUtil_Error1());
+      return;
     }
-
-    public ApiClient getDefaultClient() {
-        return defaultClient;
+    String apiKey = getApiKey(credId, run);
+    if (apiKey == null) {
+      listener.getLogger().println(Messages.DctSDkUtil_Error2(credId));
+      return;
     }
+    this.defaultClient = Configuration.getDefaultApiClient();
+    this.defaultClient.setVerifyingSsl(!DelphixGlobalConfiguration.get().getSslCheck());
+    this.defaultClient.setConnectTimeout(Constant.TIMEOUT);
+    this.defaultClient.setReadTimeout(Constant.TIMEOUT);
+    this.defaultClient.setWriteTimeout(Constant.TIMEOUT);
+    this.defaultClient.setUserAgent(Constant.USER_AGENT);
+    this.defaultClient.addDefaultHeader(Constant.CLIENT_NAME_HEADER, Constant.CLIENT_NAME);
+    this.defaultClient.setApiKey(apiKey);
+    this.defaultClient.setBasePath(url + Constant.API_VERSION);
+  }
 
-    /**
-     * 
-     * @param provisionVDBFromBookmarkParameters
-     * @return
-     * @throws ApiException
-     */
-    public ProvisionVDBResponse provisionVdbFromBookmark(
-            ProvisionVDBFromBookmarkParameters provisionVDBFromBookmarkParameters)
-            throws ApiException {
-        ProvisionVDBResponse result = null;
-        VdbsApi apiInstance = new VdbsApi(this.defaultClient);
-        result = apiInstance.provisionVdbFromBookmark(provisionVDBFromBookmarkParameters);
-        return result;
-    }
+  public ApiClient getDefaultClient() {
+    return defaultClient;
+  }
 
-    /**
-     * 
-     * @param provisionVDBBySnapshotParameters
-     * @return
-     * @throws ApiException
-     */
-    public ProvisionVDBResponse provisionVdbBySnapshot(
-            ProvisionVDBBySnapshotParameters provisionVDBBySnapshotParameters) throws ApiException {
-        ProvisionVDBResponse result = null;
-        VdbsApi apiInstance = new VdbsApi(this.defaultClient);
-        result = apiInstance.provisionVdbBySnapshot(provisionVDBBySnapshotParameters);
-        return result;
-    }
+  /**
+   * @param provisionVDBFromBookmarkParameters
+   * @return
+   * @throws ApiException
+   */
+  public ProvisionVDBResponse provisionVdbFromBookmark(
+      ProvisionVDBFromBookmarkParameters provisionVDBFromBookmarkParameters) throws ApiException {
+    ProvisionVDBResponse result = null;
+    VdbsApi apiInstance = new VdbsApi(this.defaultClient);
+    result = apiInstance.provisionVdbFromBookmark(provisionVDBFromBookmarkParameters);
+    return result;
+  }
 
-    /**
-     * 
-     * @param vdbId
-     * @param force
-     * @return
-     * @throws ApiException
-     */
-    public DeleteVDBResponse deleteVdb(String vdbId, Boolean force) throws ApiException {
-        VdbsApi apiInstance = new VdbsApi(this.defaultClient);
-        DeleteVDBParameters deleteVDBParameters = new DeleteVDBParameters();
-        deleteVDBParameters.setForce(force);
-        DeleteVDBResponse result = apiInstance.deleteVdb(vdbId, deleteVDBParameters);
-        return result;
+  /**
+   * @param provisionVDBBySnapshotParameters
+   * @return
+   * @throws ApiException
+   */
+  public ProvisionVDBResponse provisionVdbBySnapshot(
+      ProvisionVDBBySnapshotParameters provisionVDBBySnapshotParameters) throws ApiException {
+    ProvisionVDBResponse result = null;
+    VdbsApi apiInstance = new VdbsApi(this.defaultClient);
+    result = apiInstance.provisionVdbBySnapshot(provisionVDBBySnapshotParameters);
+    return result;
+  }
 
-    }
+  /**
+   * @param vdbId
+   * @param force
+   * @return
+   * @throws ApiException
+   */
+  public DeleteVDBResponse deleteVdb(String vdbId, Boolean force) throws ApiException {
+    VdbsApi apiInstance = new VdbsApi(this.defaultClient);
+    DeleteVDBParameters deleteVDBParameters = new DeleteVDBParameters();
+    deleteVDBParameters.setForce(force);
+    DeleteVDBResponse result = apiInstance.deleteVdb(vdbId, deleteVDBParameters);
+    return result;
 
-    public VDB getVDBDetails(String vdbId) throws ApiException {
-        VdbsApi apiInstance = new VdbsApi(this.defaultClient);
-        VDB result = apiInstance.getVdbById(vdbId);
-        return result;
-    }
+  }
 
-    public Job getJobStatus(String jobId) throws ApiException {
-        JobsApi apiInstance = new JobsApi(defaultClient);
-        Job result = apiInstance.getJobById(jobId);
-        return result;
-    }
+  public VDB getVDBDetails(String vdbId) throws ApiException {
+    VdbsApi apiInstance = new VdbsApi(this.defaultClient);
+    VDB result = apiInstance.getVdbById(vdbId);
+    return result;
+  }
 
-    public SearchVDBsResponse searchVDB(String name) throws ApiException {
-        VdbsApi apiInstance = new VdbsApi(this.defaultClient);
-        Integer limit = 100;
-        String cursor = null;
-        String sort = "id";
-        PermissionEnum permission = PermissionEnum.fromValue("READ");
-        SearchBody searchBody = new SearchBody();
-        String filterExpr = "name EQ \'" + name + "\'";
-        searchBody.setFilterExpression(filterExpr);
-        SearchVDBsResponse result = apiInstance.searchVdbs(limit, cursor, sort, permission, searchBody);
-        return result;
-    }
+  public Job getJobStatus(String jobId) throws ApiException {
+    JobsApi apiInstance = new JobsApi(defaultClient);
+    Job result = apiInstance.getJobById(jobId);
+    return result;
+  }
+
+  public SearchVDBsResponse searchVDB(String name) throws ApiException {
+    VdbsApi apiInstance = new VdbsApi(this.defaultClient);
+    Integer limit = 100;
+    String cursor = null;
+    String sort = "id";
+    PermissionEnum permission = PermissionEnum.fromValue("READ");
+    SearchBody searchBody = new SearchBody();
+    String filterExpr = "name EQ \'" + name + "\'";
+    searchBody.setFilterExpression(filterExpr);
+    SearchVDBsResponse result = apiInstance.searchVdbs(limit, cursor, sort, permission, searchBody);
+    return result;
+  }
 }
--- src/test/java/io/jenkins/plugins/delphix/ProvisionVDBFromBookmarkTest.java
+++ src/test/java/io/jenkins/plugins/delphix/ProvisionVDBFromBookmarkTest.java
@@ -1,164 +1,65 @@
-// package io.jenkins.plugins.delphix;
-
-// import com.cloudbees.plugins.credentials.Credentials;
-// import com.cloudbees.plugins.credentials.CredentialsProvider;
-// import com.cloudbees.plugins.credentials.CredentialsScope;
-// import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
-// import com.cloudbees.plugins.credentials.domains.Domain;
-// import com.gargoylesoftware.htmlunit.html.HtmlPage;
-// import hudson.ExtensionList;
-// import hudson.Functions;
-// import hudson.model.Descriptor;
-// import hudson.model.FreeStyleBuild;
-// import hudson.model.FreeStyleProject;
-// import hudson.model.Result;
-// import hudson.tasks.BatchFile;
-// import hudson.tasks.Builder;
-// import hudson.tasks.Shell;
-// import hudson.util.Secret;
-// import jenkins.model.GlobalConfiguration;
-// import jenkins.model.GlobalPluginConfiguration;
-// import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
-// import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-// import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-// import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-// import org.junit.Assert;
-// import org.junit.Before;
-// import org.junit.Rule;
-// import org.junit.Test;
-// import org.junit.jupiter.api.Timeout;
-// import org.jvnet.hudson.test.JenkinsRule;
-
-// import java.util.Arrays;
-// import java.util.Collections;
-// import java.util.concurrent.TimeUnit;
-// import static org.hamcrest.MatcherAssert.assertThat;
-// import static org.hamcrest.Matchers.is;
-// import static org.junit.jupiter.api.Timeout.ThreadMode.SEPARATE_THREAD;
-
-// public class ProvisionVDBFromBookmarkTest {
-
-// @Rule
-// public JenkinsRule jenkins = new JenkinsRule();
-
-// @Before
-// public void init() throws Exception {
-// DelphixGlobalConfiguration globalConfig1 =
-// GlobalConfiguration.all().get(DelphixGlobalConfiguration.class);
-// globalConfig1.setDctUrl("https://dct6.dlpxdc.co/v3");
+package io.jenkins.plugins.delphix;
 
 
-// globalConfig1.save();
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.util.Secret;
+import jenkins.model.GlobalConfiguration;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import org.junit.*;
+import org.jvnet.hudson.test.JenkinsRule;
 
-// StringCredentialsImpl c = new StringCredentialsImpl(CredentialsScope.USER, "test123",
-// "description", Secret.fromString(
-// "apk 1.YKhbbGsoA2LUoaIpZ8nxPQsOQbQ5BBWAdB7AhWZISkGjeB6JsyiImpRP0EtKG86y"));
-// CredentialsProvider.lookupStores(jenkins).iterator().next().addCredentials(Domain.global(),
-// c);
-// }
+public class ProvisionVDBFromBookmarkTest {
 
+  @Rule
+  public JenkinsRule jenkins = new JenkinsRule();
 
-// // @Test
-// // public void testConfigRoundtrip() throws Exception {
-// // FreeStyleProject project = jenkins.createFreeStyleProject();
-
-// // ProvisionVDBFromBookmark x = new ProvisionVDBFromBookmark("GLOBAL_CREDENTIALS_ID_1");
-// // x.setCredentialId("test123");
-// // project.getBuildersList().add(x);
-// // project = jenkins.configRoundtrip(project);
-// // FreeStyleBuild build =
-// // jenkins.assertBuildStatus(Result.SUCCESS, project.scheduleBuild2(0).get());
-// // build.ge
-// // jenkins.assertEqualDataBoundBeans(x, project.getBuildersList().get(0));
-// // }
-
-// // @Test
-// // public void publishArtifactSuccessTest() throws Exception {
+  @Before
+  public void init() throws Exception {
+    DelphixGlobalConfiguration globalConfig1 =
+        GlobalConfiguration.all().get(DelphixGlobalConfiguration.class);
+    globalConfig1.setDctUrl("https://ubuntu-2-uv49-qar-125346-27a4593a.dlpxdc.co");
+    globalConfig1.setSslCheck(true);
 
 
-// // FreeStyleProject project = jenkins.createFreeStyleProject();
-// // ProvisionVDBFromBookmark step = new ProvisionVDBFromBookmark("GLOBAL_CREDENTIALS_ID_1");
-// // step.setCredentialId("test123");
-// // project.getBuildersList().add(step);
-// // FreeStyleBuild build = project.scheduleBuild2(0).get();
-// // System.out.println(build.getDisplayName() + " completed");
-// // // TODO: change this to use HtmlUnit
-// // // String s = FileUtils.readFileToString(build.getLogFile());
-// // // assertThat(s, containsString("Finished: SUCCESS"));
-// // }
+    globalConfig1.save();
 
-// // @Test
-// // public void testBuild() throws Exception {
-// // FreeStyleProject project = jenkins.createFreeStyleProject();
-// // ProvisionVDBFromBookmark builder = new ProvisionVDBFromBookmark("GLOBAL_CREDENTIALS_ID_1");
-// // builder.setCredentialId("test123");
-// // project.getBuildersList().add(builder);
-// // FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
-// // jenkins.assertLogContains("web test run " + "\n", build);
-// // }
+    StringCredentialsImpl c =
+        new StringCredentialsImpl(CredentialsScope.USER, "test123", "description",
+            Secret.fromString(
+                "apk 1.jTElhpXIao7pTNzVCYdkj1HpGXriTBlYbPha1Di8HjvMF6nESA1crkGlljowDs7y"));
+    CredentialsProvider.lookupStores(jenkins).iterator().next().addCredentials(Domain.global(), c);
+  }
 
-// // @Test
-// // @Timeout(value = 10, unit = TimeUnit.MINUTES, threadMode = SEPARATE_THREAD)
-// // // @Timeout(value = 10, unit = TimeUnit.MINUTES)
-// // public void testBuild1() throws Exception {
-// // FreeStyleProject project = jenkins.createFreeStyleProject();
-// // ProvisionVDBFromBookmark builder = new ProvisionVDBFromBookmark("bkm123");
-// // builder.setCredentialId("test123");
-// // builder.setAutoSelectRepository(true);
-// // // builder.setSkipPolling(true);
-// // project.getBuildersList().add(builder);
-// // FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
-// // jenkins.assertLogContains("Current Job Status: COMPLETED", build);
-// // // System.out.println(build);
-// // }
+  @Test
+  public void testProvisionWithPolling() throws Exception {
+    FreeStyleProject project = jenkins.createFreeStyleProject();
+    ProvisionVDBFromBookmark builder =
+        new ProvisionVDBFromBookmark("5057a92229644028bee9206f4f32965c");
+    //    builder.setSourceDataId("4-ORACLE_DB_CONTAINER-6");
+    builder.setCredentialId("test123");
+    builder.setAutoSelectRepository(true);
+    builder.setName("vdb_test_1");
+    String jsonString =
+        "{\"appdata_source_params\": {\"mountLocation\": \"/dlpx_mnt\", \"postgresPort\": 5435}}";
+    builder.setJsonParam(jsonString);
+    project.getBuildersList().add(builder);
+    FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
+    jenkins.assertLogContains("Current Job Status: COMPLETED", build);
+  }
 
-// // @Test
-// // public void testConfigElements() throws Exception {
-// // // HtmlPage page = jenkins.createWebClient().goTo("configure");
-// // // String pageText = page.asNormalizedText();
-// // // Assert.assertTrue("Missing: BrowserStack Global Config",
-// // // pageText.contains("BrowserStack"));
-// // }
+  @After
+  public void destroy() throws Exception {
+    FreeStyleProject project = jenkins.createFreeStyleProject();
+    DeleteVDB builder1 = new DeleteVDB();
+    builder1.setName("vdb_test_1");
+    builder1.setCredentialId("test123");
+    project.getBuildersList().add(builder1);
+    FreeStyleBuild build1 = jenkins.buildAndAssertSuccess(project);
+    jenkins.assertLogContains("Current Job Status: COMPLETED", build1);
 
-
-// // @Test
-// // public void testBuild() throws Exception {
-// // FreeStyleProject project = jr.createFreeStyleProject();
-// // ProvarAutomation builder = new ProvarAutomation(provarAutomationName, buildFile, testPlan,
-// // testFolder, environment, browser, secretsPassword, salesforceMetadataCacheSetting,
-// // resultsPathSetting, projectName);
-// // project.getBuildersList().add(builder);
-// // FreeStyleBuild build =
-// // jr.assertBuildStatus(Result.FAILURE, project.scheduleBuild2(quietPeriod).get());
-// // jr.assertLogContains("Running the build file: " + buildFile, build);
-// // jr.assertLogContains("Executing test plan: " + testPlan, build);
-// // jr.assertLogContains("Executing test folder: " + testFolder, build);
-// // jr.assertLogContains("Target browser: " + browser, build);
-// // jr.assertLogContains("Target environment: " + environment, build);
-// // jr.assertLogContains("Salesforce Metadata Cache Setting: " + salesforceMetadataCacheSetting,
-// // build);
-// // jr.assertLogContains("Results Path Setting: " + resultsPathSetting, build);
-// // jr.assertLogContains("Project Folder: " + projectName, build);
-// // jr.assertLogContains("Project is encrypted! Thank you for being secure.", build);
-// // }
-
-// // @Test
-// // public void testScriptedPipeline() throws Exception {
-// // String agentLabel = "my-agent";
-// // jr.createOnlineSlave(Label.get(agentLabel));
-// // WorkflowJob job = jr.createProject(WorkflowJob.class, "test-scripted-pipeline");
-// // String pipelineScript = "node {\n" + " provarAutomation provarAutomationName: '"
-// // + provarAutomationName + "',\n" + " buildFile: '" + buildFile + "',\n"
-// // + " testPlan: '" + testPlan + "',\n" + " testFolder: '" + testFolder + "',\n"
-// // + " environment: '" + environment + "',\n" + " browser: '" + browser + "',\n"
-// // + " secretsPassword: '" + secretsPassword + "',\n"
-// // + " salesforceMetadataCacheSetting: '" + salesforceMetadataCacheSetting + "',\n"
-// // + " resultsPathSetting: '" + resultsPathSetting + "',\n" + " projectName: '"
-// // + projectName + "'\n" + "}";
-// // job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
-
-// // WorkflowRun completedBuild = job.scheduleBuild2(quietPeriod).get();
-// // jr.assertBuildStatusSuccess(completedBuild);
-// // jr.assertLogContains("Start of Pipeline", completedBuild);
-// // }
-// }
+  }
+}
